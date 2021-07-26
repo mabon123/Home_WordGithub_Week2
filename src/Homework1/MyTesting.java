b881642febd10a3541a5ba40694bc939316fcf89
mabon123 <87595919+mabon123@users.noreply.github.com>
2021-07-26 18:19:34 +0700

    Create MyTesting File To commit staging

---
+++ src/Homework1/MyTesting.java
@@ -0,0 +1,7 @@
+package Homework1;
+
+public class MyTesting {
+    public static void main(String[] args) {
+        System.out.println("My Homework Github................");
+    }
+}
--- src/Homework1/Testing.java
+++ src/Homework1/Testing.java
@@ -1,7 +1,4 @@
 package Homework1;
 
 public class Testing {
-    public static void main(String[] args) {
-        System.out.println("Hello Word.........");
-    }
 }
